795abb67d4a6893c29d85281e77dcf9932440a68
Binayak-Pokhrel <binayakpokhrel124@gmail.com>
2022-12-14 11:42:43 +0545

    first push py file

---
+++ 1.py
@@ -0,0 +1,47 @@
+'''a = input('Enter your name\n')
+print('hello',a,', have a nice day')'''
+
+
+'''
+x = int(input('Enter value for x: '))
+y = int(input('Enter value for y: '))
+
+print(x**3+3*(x**2)*y+3*x*(y**2)+y**3)
+'''
+
+'''
+#for area of circle
+r = int(input('Enter the value for radius'))
+pi = (22/7)
+A = pi*(r**2)
+print (A)
+'''
+
+#for area = l*b and permeter of rectangle = 2(L+B)
+'''l = int(input('Length\n'))
+b = int(input('Breadth\n'))
+Area = l*b
+Perimeter = 2*(l+b)
+
+print('The area is',Area,'\nThe perimeter is',Perimeter)
+'''
+'''
+#for 8
+a=int(input("Value of a"))
+b=int(input('value of b'))
+c=int(input('value of c'))
+x = ((-b - ((b**2)-4*a*c)**(1/2)))/(2*a)
+y = ((-b + ((b**2)-4*a*c)**(1/2)))/(2*a)
+
+print(x)
+print(y)
+'''
+
+#swapping the number
+a = int(input('first no.'))
+b = int(input('Second no.'))
+temp=a
+a = b
+b = temp
+print('a: ',a)
+print('b =',b)
